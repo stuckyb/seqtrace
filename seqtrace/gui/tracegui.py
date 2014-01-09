8bb2aa21baf70b08998096b26514c661d148407d
stucky.brian <stucky.brian@gmail.com>
2014-01-09 05:59:25 +0000

    Added the ability to copy selections on the consensus sequence in the trace view window to the system clipboard, using pyperclip for cross-platform clipboard access.

--- seqtrace/gui/tracegui.py
+++ seqtrace/gui/tracegui.py
@@ -22,6 +22,8 @@ from seqtrace.core.observable import Observable
 from seqtrace.gui.dialgs import CommonDialogs, EntryDialog
 from seqtrace.gui.statusbar import ConsensSeqStatusBar
 
+import seqtrace.gui.pyperclip as pyperclip
+
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -1089,6 +1091,7 @@ class TraceWindow(gtk.Window, CommonDialogs, Observable):
             <menuitem action="Undo" />
             <menuitem action="Redo" />
             <separator />
+            <menuitem action="Copy" />
             <menuitem action="Delete" />
             <menuitem action="Modify" />
             <separator />
@@ -1096,6 +1099,7 @@ class TraceWindow(gtk.Window, CommonDialogs, Observable):
         </menu>
         </menubar>
         <popup name="editpopup">
+            <menuitem action="Copy" />
             <menuitem action="Delete" />
             <menuitem action="Modify" />
             <separator />
@@ -1109,6 +1113,7 @@ class TraceWindow(gtk.Window, CommonDialogs, Observable):
             <toolitem action="Undo" />
             <toolitem action="Redo" />
             <separator />
+            <toolitem action="Copy" />
             <toolitem action="Delete" />
             <toolitem action="Modify" />
         </toolbar>'''
@@ -1135,6 +1140,7 @@ class TraceWindow(gtk.Window, CommonDialogs, Observable):
         # these actions are only enabled when there is an active selection
         self.sel_edit_ag = gtk.ActionGroup('selected_edit_actions')
         self.sel_edit_ag.add_actions([
+            ('Copy', gtk.STOCK_COPY, '_Copy selected base(s) to clipboard', None, 'Copy the selected base(s) to the system clipboard', self.copyConsBases),
             ('Delete', gtk.STOCK_DELETE, '_Delete selected base(s)', None, 'Delete the selected base(s) from the working sequence', self.deleteConsBases),
             ('Modify', gtk.STOCK_EDIT, '_Modify selected base(s)...', None, 'Edit the selected base(s)', self.editConsBases)])
 
@@ -1301,6 +1307,13 @@ class TraceWindow(gtk.Window, CommonDialogs, Observable):
     def redoConsChange(self, widget):
         self.cons.redo()
         self.setSaveEnabled(True)
+
+    def copyConsBases(self, widget):
+        csv = self.consview.getConsensusSequenceViewer()
+        sel = csv.getSelection()
+        seq = self.cons.getConsensus(sel[0], sel[1])
+
+        pyperclip.copy(seq)
 
     def deleteConsBases(self, widget):
         csv = self.consview.getConsensusSequenceViewer()
