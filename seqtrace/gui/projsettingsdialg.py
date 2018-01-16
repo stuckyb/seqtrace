5f5d1e72097655ab7214bf756542e7ce68324ed6
stuckyb <stuckyb@flmnh.ufl.edu>
2018-01-16 11:25:19 -0500

    Ported project settings interface to GTK+3.

--- seqtrace/gui/projsettingsdialg.py
+++ seqtrace/gui/projsettingsdialg.py
@@ -18,19 +18,19 @@
 from seqtrace.core.consens import ConsensSeqBuilder
 from seqtrace.gui.dialgs import CommonDialogs
 
-import pygtk
-pygtk.require('2.0')
-import gtk
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 import os.path
 import re
 
 
-class ProjectSettingsDialog(gtk.Dialog, CommonDialogs):
+class ProjectSettingsDialog(Gtk.Dialog, CommonDialogs):
     def __init__(self, project):
-        gtk.Dialog.__init__(self, 'Project Settings', None, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                (gtk.STOCK_OK, gtk.RESPONSE_OK, gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL))
-        self.set_default_response(gtk.RESPONSE_OK)
+        Gtk.Dialog.__init__(self, 'Project Settings', None, Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                (Gtk.STOCK_OK, Gtk.ResponseType.OK, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
+        self.set_default_response(Gtk.ResponseType.OK)
 
         self.project = project
 
@@ -40,213 +40,218 @@ class ProjectSettingsDialog(gtk.Dialog, CommonDialogs):
         tabpadding_sides = 18
 
         # Set up the tabs ("notebook") for the project settings.
-        nb = gtk.Notebook()
+        nb = Gtk.Notebook()
         nb.set_border_width(8)
-        nb.set_tab_pos(gtk.POS_TOP)
+        nb.set_tab_pos(Gtk.PositionType.TOP)
 
         # Create the UI elements for the trace file settings tab.
-        mainvb = gtk.VBox(False, 16)
-        tracevb = gtk.VBox(False, 20)
+        mainvb = Gtk.VBox(False, 16)
+        tracevb = Gtk.VBox(False, 20)
         tracevb.set_border_width(insideframe_padding)
 
         # Get the consensus sequence settings from the project.
         cssettings = self.project.getConsensSeqSettings()
 
         # Set up UI components for the trace file location.
-        vb = gtk.VBox()
-        tf_hb1 = gtk.HBox()
-        tf_hb1.pack_start(gtk.Label('Location of trace files:'), False)
-        tf_hb2 = gtk.HBox()
-        self.fc_entry = gtk.Entry()
+        vb = Gtk.VBox()
+        tf_hb1 = Gtk.HBox()
+        tf_hb1.pack_start(Gtk.Label('Location of trace files:'), False, True, 0)
+        tf_hb2 = Gtk.HBox()
+        self.fc_entry = Gtk.Entry()
         self.fc_entry.set_width_chars(40)
         self.fc_entry.set_text(self.project.getTraceFileDir())
-        tf_hb2.pack_start(self.fc_entry)
+        tf_hb2.pack_start(self.fc_entry, True, True, 0)
 
-        # use the STOCK_DIRECTORY icon for the button
-        gtk.stock_add([('choose_dir', 'C_hoose...', 0, 0, None)])
-        icfact = gtk.IconFactory()
-        icfact.add_default()
-        icfact.add('choose_dir', self.get_style().lookup_icon_set(gtk.STOCK_DIRECTORY))
-        fc_button = gtk.Button(stock='choose_dir')
+        # Use the default "folder" icon for the button (formerly
+        # STOCK_DIRECTORY with the deprecated stock item system).
+        iconimg = Gtk.Image.new_from_icon_name('folder', Gtk.IconSize.BUTTON)
+        fc_button = Gtk.Button.new_with_mnemonic(' C_hoose...')
+        fc_button.set_image(iconimg)
         fc_button.connect('clicked', self.chooseDirectory, self.fc_entry)
 
         #fc_button.set_label('Choose...')
-        tf_hb2.pack_start(fc_button)
-        vb.pack_start(tf_hb1)
-        vb.pack_start(tf_hb2)
+        tf_hb2.pack_start(fc_button, True, True, 0)
+        vb.pack_start(tf_hb1, True, True, 0)
+        vb.pack_start(tf_hb2, True, True, 0)
 
-        self.fc_checkbox = gtk.CheckButton('Use relative folder path')
+        self.fc_checkbox = Gtk.CheckButton('Use relative folder path')
         if os.path.isabs(self.project.getTraceFileDir()):
             self.fc_checkbox.set_active(False)
         else:
             self.fc_checkbox.set_active(True)
         self.fc_cb_hid = self.fc_checkbox.connect('toggled', self.useRelPathToggled)
-        vb.pack_start(self.fc_checkbox)
+        vb.pack_start(self.fc_checkbox, True, True, 0)
 
-        tracevb.pack_start(vb)
+        tracevb.pack_start(vb, True, True, 0)
 
         # Set up UI components for the forward/reverse trace file search strings.
-        vb = gtk.VBox()
-        tfs_hb1 = gtk.HBox()
-        tfs_hb1.pack_start(gtk.Label('Search strings for identifying forward and reverse trace files:'), False)
+        vb = Gtk.VBox()
+        tfs_hb1 = Gtk.HBox()
+        tfs_hb1.pack_start(Gtk.Label('Search strings for identifying forward and reverse trace files:'), False, True, 0)
 
         # Create a layout table for the labels and text entries.
-        table = gtk.Table(2, 2)
+        table = Gtk.Table(2, 2)
 
-        self.tfs_fwd_entry = gtk.Entry()
+        self.tfs_fwd_entry = Gtk.Entry()
         self.tfs_fwd_entry.set_width_chars(12)
         self.tfs_fwd_entry.set_text(self.project.getFwdTraceSearchStr())
-        table.attach(gtk.Label('Forward: '), 0, 1, 0, 1, xoptions=0)
+        table.attach(Gtk.Label(label='Forward: '), 0, 1, 0, 1, xoptions=0)
         table.attach(self.tfs_fwd_entry, 1, 2, 0, 1, xoptions=0)
         
-        self.tfs_rev_entry = gtk.Entry()
+        self.tfs_rev_entry = Gtk.Entry()
         self.tfs_rev_entry.set_width_chars(12)
         self.tfs_rev_entry.set_text(self.project.getRevTraceSearchStr())
-        table.attach(gtk.Label('Reverse: '), 0, 1, 1, 2, xoptions=0)
+        table.attach(Gtk.Label(label='Reverse: '), 0, 1, 1, 2, xoptions=0)
         table.attach(self.tfs_rev_entry, 1, 2, 1, 2, xoptions=0)
 
-        vb.pack_start(tfs_hb1)
-        vb.pack_start(table)
+        vb.pack_start(tfs_hb1, True, True, 0)
+        vb.pack_start(table, True, True, 0)
 
-        tracevb.pack_start(vb)
+        tracevb.pack_start(vb, True, True, 0)
 
-        frame = gtk.Frame('Trace files')
+        frame = Gtk.Frame(label='Trace files')
         frame.add(tracevb)
-        mainvb.pack_start(frame)
+        mainvb.pack_start(frame, True, True, 0)
 
         # Set up UI components for the forward/reverse primer strings.
-        vb = gtk.VBox()
-        vb = gtk.VBox(False, 20)
+        vb = Gtk.VBox()
+        vb = Gtk.VBox(False, 20)
         vb.set_border_width(insideframe_padding)
 
         # Create a layout table for the labels and text entries.
-        table = gtk.Table(2, 2)
+        table = Gtk.Table(2, 2)
 
-        self.fwdprimer_entry = gtk.Entry()
+        self.fwdprimer_entry = Gtk.Entry()
         self.fwdprimer_entry.set_width_chars(38)
         self.fwdprimer_entry.set_text(cssettings.getForwardPrimer())
-        table.attach(gtk.Label('Forward primer: '), 0, 1, 0, 1, xoptions=0)
+        table.attach(Gtk.Label(label='Forward primer: '), 0, 1, 0, 1, xoptions=0)
         table.attach(self.fwdprimer_entry, 1, 2, 0, 1, xoptions=0)
         
-        self.revprimer_entry = gtk.Entry()
+        self.revprimer_entry = Gtk.Entry()
         self.revprimer_entry.set_width_chars(38)
         self.revprimer_entry.set_text(cssettings.getReversePrimer())
-        table.attach(gtk.Label('Reverse primer: '), 0, 1, 1, 2, xoptions=0)
+        table.attach(Gtk.Label(label='Reverse primer: '), 0, 1, 1, 2, xoptions=0)
         table.attach(self.revprimer_entry, 1, 2, 1, 2, xoptions=0)
 
-        vb.pack_start(table)
+        vb.pack_start(table, True, True, 0)
 
-        frame = gtk.Frame("Primer sequences (5' to 3')")
+        frame = Gtk.Frame(label="Primer sequences (5' to 3')")
         frame.add(vb)
-        mainvb.pack_start(frame)
+        mainvb.pack_start(frame, True, True, 0)
 
         # Use another VBox to get extra padding on the sides.
-        vbpad = gtk.VBox(False, 0)
+        vbpad = Gtk.VBox(False, 0)
         vbpad.set_border_width(tabpadding_sides)
-        vbpad.pack_start(mainvb, expand=False, padding=(tabpadding_tops - tabpadding_sides))
+        vbpad.pack_start(mainvb, False, True, tabpadding_tops - tabpadding_sides)
 
-        nb.append_page(vbpad, gtk.Label('Trace settings'))
+        nb.append_page(vbpad, Gtk.Label(label='Trace settings'))
 
         # Create the UI components for the sequence processing tab.
-        mainvb = gtk.VBox(False, 16)
+        mainvb = Gtk.VBox(False, 16)
 
         # Set up UI components for choosing the confidence score cutoff value.
-        vb = gtk.VBox(False, 12)
+        vb = Gtk.VBox(False, 12)
         vb.set_border_width(insideframe_padding)
-        hb1 = gtk.HBox()
-        hb1.pack_start(gtk.Label('Min. confidence score:  '), False)
+        hb1 = Gtk.HBox()
+        hb1.pack_start(Gtk.Label('Min. confidence score:  '), False, True, 0)
 
-        self.ph_adj = gtk.Adjustment(cssettings.getMinConfScore(), 1, 61, 1)
-        spin = gtk.SpinButton(self.ph_adj, 0, 0)
-        hb1.pack_start(spin, False, False)
+        self.ph_adj = Gtk.Adjustment(cssettings.getMinConfScore(), 1, 61, 1)
+        spin = Gtk.SpinButton(adjustment=self.ph_adj, climb_rate=0, digits=0)
+        hb1.pack_start(spin, False, False, 0)
 
-        vb.pack_start(hb1)
+        vb.pack_start(hb1, True, True, 0)
 
         # Create the UI components for choosing a consensus algorithm.
-        hb1 = gtk.HBox()
-        hb1.pack_start(gtk.Label('Consensus algorithm:  '), False)
-        self.cons_bayes_rb = gtk.RadioButton(None, 'Bayesian   ')
-        hb1.pack_start(self.cons_bayes_rb, False)
-        self.cons_legacy_rb = gtk.RadioButton(self.cons_bayes_rb, 'SeqTrace 0.8')
+        hb1 = Gtk.HBox()
+        hb1.pack_start(Gtk.Label('Consensus algorithm:  '), False, True, 0)
+        self.cons_bayes_rb = Gtk.RadioButton(label='Bayesian   ')
+        hb1.pack_start(self.cons_bayes_rb, False, True, 0)
+        self.cons_legacy_rb = Gtk.RadioButton(group=self.cons_bayes_rb, label='SeqTrace 0.8')
 
         if cssettings.getConsensusAlgorithm() == 'Bayesian':
             self.cons_bayes_rb.set_active(True)
         else:
             self.cons_legacy_rb.set_active(True)
 
-        hb1.pack_start(self.cons_legacy_rb)
+        hb1.pack_start(self.cons_legacy_rb, True, True, 0)
 
-        vb.pack_start(hb1)
+        vb.pack_start(hb1, True, True, 0)
 
-        frame = gtk.Frame('Consensus settings')
+        frame = Gtk.Frame(label='Consensus settings')
         frame.add(vb)
-        mainvb.pack_start(frame)
+        mainvb.pack_start(frame, True, True, 0)
 
         # Set up UI components for sequence trimming settings.
-        vb = gtk.VBox(False, 24)
+        vb = Gtk.VBox(False, 24)
         vb.set_border_width(insideframe_padding)
-        self.autotrim_checkbox = gtk.CheckButton('Automatically trim sequence ends')
+        self.autotrim_checkbox = Gtk.CheckButton('Automatically trim sequence ends')
         self.autotrim_checkbox.connect('toggled', self.autoTrimToggled)
-        vb.pack_start(self.autotrim_checkbox)
+        vb.pack_start(self.autotrim_checkbox, True, True, 0)
 
         # Create UI components for primer trimming.
-        vb2 = gtk.VBox(False, 6)
-        hb2 = gtk.HBox()
-        self.trimprimers_checkbox = gtk.CheckButton('Trim primers if ')
+        vb2 = Gtk.VBox(False, 6)
+        hb2 = Gtk.HBox()
+        self.trimprimers_checkbox = Gtk.CheckButton('Trim primers if ')
         self.trimprimers_checkbox.set_active(cssettings.getTrimPrimers())
-        hb2.pack_start(self.trimprimers_checkbox, False)
-        self.primermatch_th_adj = gtk.Adjustment(int(cssettings.getPrimerMatchThreshold() * 100), 1, 100, 1)
-        self.primermatch_th_spin = gtk.SpinButton(self.primermatch_th_adj, 0, 0)
-        hb2.pack_start(self.primermatch_th_spin, False, False)
-        self.trimprimers_label = gtk.Label(' % of the primer alignment matches.')
-        hb2.pack_start(self.trimprimers_label, False)
+        hb2.pack_start(self.trimprimers_checkbox, False, True, 0)
+        self.primermatch_th_adj = Gtk.Adjustment(int(cssettings.getPrimerMatchThreshold() * 100), 1, 100, 1)
+        self.primermatch_th_spin = Gtk.SpinButton(
+            adjustment=self.primermatch_th_adj, climb_rate=0, digits=0
+        )
+        hb2.pack_start(self.primermatch_th_spin, False, False, 0)
+        self.trimprimers_label = Gtk.Label(label=' % of the primer alignment matches.')
+        hb2.pack_start(self.trimprimers_label, False, True, 0)
 
-        vb2.pack_start(hb2)
+        vb2.pack_start(hb2, True, True, 0)
 
         # Check box for end gap trimming.
-        self.trimgaps_checkbox = gtk.CheckButton('Trim alignment end gap regions')
+        self.trimgaps_checkbox = Gtk.CheckButton('Trim alignment end gap regions')
         self.trimgaps_checkbox.set_active(cssettings.getTrimEndGaps())
-        vb2.pack_start(self.trimgaps_checkbox)
+        vb2.pack_start(self.trimgaps_checkbox, True, True, 0)
 
         qualtrim_winsize, qualtrim_basecnt = cssettings.getQualityTrimParams()
 
         # Check box and spin buttons for end quality trimming.
-        hb2 = gtk.HBox()
-        self.qualtrim_checkbox = gtk.CheckButton('Trim until at least ')
+        hb2 = Gtk.HBox()
+        self.qualtrim_checkbox = Gtk.CheckButton('Trim until at least ')
         self.qualtrim_checkbox.set_active(cssettings.getDoQualityTrim())
-        hb2.pack_start(self.qualtrim_checkbox, False)
-        self.qualtrim_basecnt_adj = gtk.Adjustment(qualtrim_basecnt, 1, qualtrim_winsize, 1)
-        self.qualtrim_basecnt_spin = gtk.SpinButton(self.qualtrim_basecnt_adj, 0, 0)
-        hb2.pack_start(self.qualtrim_basecnt_spin, False, False)
+        hb2.pack_start(self.qualtrim_checkbox, False, True, 0)
+        self.qualtrim_basecnt_adj = Gtk.Adjustment(qualtrim_basecnt, 1, qualtrim_winsize, 1)
+        self.qualtrim_basecnt_spin = Gtk.SpinButton(
+            adjustment=self.qualtrim_basecnt_adj, climb_rate=0, digits=0
+        )
+        hb2.pack_start(self.qualtrim_basecnt_spin, False, False, 0)
 
-        self.qualtrim_label1 = gtk.Label(' out of ')
-        hb2.pack_start(self.qualtrim_label1, False)
-        self.qualtrim_winsize_adj = gtk.Adjustment(qualtrim_winsize, 1, 20, 1)
-        self.qualtrim_winsize_spin = gtk.SpinButton(self.qualtrim_winsize_adj, 0, 0)
-        hb2.pack_start(self.qualtrim_winsize_spin, False, False)
+        self.qualtrim_label1 = Gtk.Label(label=' out of ')
+        hb2.pack_start(self.qualtrim_label1, False, True, 0)
+        self.qualtrim_winsize_adj = Gtk.Adjustment(qualtrim_winsize, 1, 20, 1)
+        self.qualtrim_winsize_spin = Gtk.SpinButton(
+            adjustment=self.qualtrim_winsize_adj, climb_rate=0, digits=0
+        )
+        hb2.pack_start(self.qualtrim_winsize_spin, False, False, 0)
         self.qualtrim_winsize_adj.connect('value_changed', self.autoTrimWinSizeChanged)
 
-        self.qualtrim_label2 = gtk.Label(' bases are correctly called.')
-        hb2.pack_start(self.qualtrim_label2, False)
-        vb2.pack_start(hb2)
+        self.qualtrim_label2 = Gtk.Label(label=' bases are correctly called.')
+        hb2.pack_start(self.qualtrim_label2, False, True, 0)
+        vb2.pack_start(hb2, True, True, 0)
 
         self.autotrim_checkbox.set_active(cssettings.getTrimConsensus())
         self.autotrim_checkbox.toggled()
 
-        vb.pack_start(vb2)
-        frame = gtk.Frame('Sequence trimming')
+        vb.pack_start(vb2, True, True, 0)
+        frame = Gtk.Frame(label='Sequence trimming')
         frame.add(vb)
 
-        mainvb.pack_start(frame)
+        mainvb.pack_start(frame, True, True, 0)
 
         # Use another VBox to get extra padding on the sides.
-        vbpad = gtk.VBox(False, 0)
+        vbpad = Gtk.VBox(False, 0)
         vbpad.set_border_width(tabpadding_sides)
-        vbpad.pack_start(mainvb, expand=False, padding=(tabpadding_tops - tabpadding_sides))
+        vbpad.pack_start(mainvb, False, True, tabpadding_tops - tabpadding_sides)
 
-        nb.append_page(vbpad, gtk.Label('Sequence processing'))
+        nb.append_page(vbpad, Gtk.Label(label='Sequence processing'))
 
-        self.vbox.pack_start(nb)
+        self.vbox.pack_start(nb, True, True, 0)
 
         self.vbox.show_all()
 
@@ -352,13 +357,13 @@ class ProjectSettingsDialog(gtk.Dialog, CommonDialogs):
 
     def chooseDirectory(self, widget, entry):
         # create a file chooser dialog to get a directory name from the user
-        fc = gtk.FileChooserDialog('Choose Trace Files Location', None, gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
-                (gtk.STOCK_OK, gtk.RESPONSE_OK, gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL))
+        fc = Gtk.FileChooserDialog('Choose Trace Files Location', None, Gtk.FileChooserAction.SELECT_FOLDER,
+                (Gtk.STOCK_OK, Gtk.ResponseType.OK, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
         fc.set_current_folder(self.project.getAbsTraceFileDir())
         response = fc.run()
         fname = fc.get_filename()
         fc.destroy()
-        if response != gtk.RESPONSE_OK:
+        if response != Gtk.ResponseType.OK:
             return
 
         if self.fc_checkbox.get_active():
