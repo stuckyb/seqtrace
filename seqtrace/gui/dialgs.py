958a6c5e2c8dc3b528563b26a0c285fddc76c635
stuckyb <stuckyb@flmnh.ufl.edu>
2018-01-17 17:19:12 -0500

    Ported common dialogs code to Gtk+3.

--- seqtrace/gui/dialgs.py
+++ seqtrace/gui/dialgs.py
@@ -15,10 +15,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import pygtk
-pygtk.require('2.0')
-import gtk
-
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 
 # constant for the "yes to all" response
@@ -26,85 +25,101 @@ YES_TO_ALL = 1
 
 
 class CommonDialogs:
-    """ A mixin class to add common message dialog functionality to classes that inherit
-    from gtk.Window.  The methods essentially act as facades to the GTK MessageDialog class. """
-
-    def showMessage(self, message, mtype=gtk.MESSAGE_ERROR):
-        msg = gtk.MessageDialog(self, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, mtype,
-                gtk.BUTTONS_OK, message)
+    """
+    A mixin class to add common message dialog functionality to classes that
+    inherit from Gtk.Window.  The methods essentially act as facades to the GTK
+    MessageDialog class.
+    """
+    def showMessage(self, message, mtype=Gtk.MessageType.ERROR):
+        msg = Gtk.MessageDialog(
+            self, Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+            mtype, Gtk.ButtonsType.OK, message
+        )
         response = msg.run()
         msg.destroy()
     
-    def showYesNoDialog(self, message, mtype=gtk.MESSAGE_QUESTION):
-        msg = gtk.MessageDialog(self, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_QUESTION,
-                gtk.BUTTONS_YES_NO, message)
+    def showYesNoDialog(self, message, mtype=Gtk.MessageType.QUESTION):
+        msg = Gtk.MessageDialog(
+            self, Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+            Gtk.MessageType.QUESTION, Gtk.ButtonsType.YES_NO, message
+        )
         response = msg.run()
         msg.destroy()
         
         return response
 
-    def showYesNoCancelDialog(self, message, mtype=gtk.MESSAGE_QUESTION):
-        msg = gtk.MessageDialog(self, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_QUESTION,
-            gtk.BUTTONS_NONE, message)
-        msg.add_buttons(gtk.STOCK_YES, gtk.RESPONSE_YES, gtk.STOCK_NO, gtk.RESPONSE_NO,
-                gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
-        msg.set_default_response(gtk.RESPONSE_CANCEL)
+    def showYesNoCancelDialog(self, message, mtype=Gtk.MessageType.QUESTION):
+        msg = Gtk.MessageDialog(
+            self, Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+            Gtk.MessageType.QUESTION, Gtk.ButtonsType.NONE, message
+        )
+        msg.add_buttons(Gtk.STOCK_YES, Gtk.ResponseType.YES, Gtk.STOCK_NO, Gtk.ResponseType.NO,
+                Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        msg.set_default_response(Gtk.ResponseType.CANCEL)
         response = msg.run()
         msg.destroy()
 
         return response
 
-    def showYesToAllDialog(self, message, mtype=gtk.MESSAGE_QUESTION):
-        """ Shows a "yes to all" dialog, which also contains "yes", "no", and "cancel" buttons. """
-
-        msg = gtk.MessageDialog(self, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_QUESTION,
-            gtk.BUTTONS_NONE, message)
-        msg.add_buttons('Yes to All', YES_TO_ALL, gtk.STOCK_YES, gtk.RESPONSE_YES,
-                gtk.STOCK_NO, gtk.RESPONSE_NO, gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
-        msg.set_default_response(gtk.RESPONSE_YES)
+    def showYesToAllDialog(self, message, mtype=Gtk.MessageType.QUESTION):
+        """
+        Shows a "yes to all" dialog, which also contains "yes", "no", and
+        "cancel" buttons.
+        """
+        msg = Gtk.MessageDialog(
+            self, Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+            Gtk.MessageType.QUESTION, Gtk.ButtonsType.NONE, message
+        )
+        msg.add_buttons('Yes to All', YES_TO_ALL, Gtk.STOCK_YES, Gtk.ResponseType.YES,
+                Gtk.STOCK_NO, Gtk.ResponseType.NO, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        msg.set_default_response(Gtk.ResponseType.YES)
         response = msg.run()
         msg.destroy()
 
         return response
-        
 
 
-class EntryDialog(gtk.Dialog):
-    """ Defines a simple text entry dialog with a single text entry box. """
-
+class EntryDialog(Gtk.Dialog):
+    """
+    Defines a simple text entry dialog with a single text entry box.
+    """
     def __init__(self, parent, title, prompt, init_text, entry_width=20):
-        gtk.Dialog.__init__(self, title, parent, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                (gtk.STOCK_OK, gtk.RESPONSE_OK, gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL))
-        self.set_default_response(gtk.RESPONSE_OK)
+        Gtk.Dialog.__init__(
+            self, title, parent,
+            Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+            (Gtk.STOCK_OK, Gtk.ResponseType.OK, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        )
+        self.set_default_response(Gtk.ResponseType.OK)
 
-        vb = gtk.VBox()
+        vb = Gtk.VBox()
         vb.set_border_width(14)
 
-        hb1 = gtk.HBox()
-        hb1.pack_start(gtk.Label(prompt), False)
-        vb.pack_start(hb1)
+        hb1 = Gtk.HBox()
+        hb1.pack_start(Gtk.Label(prompt), False, True, 0)
+        vb.pack_start(hb1, True, True, 0)
 
-        hb2 = gtk.HBox()
-        self.entry = gtk.Entry()
+        hb2 = Gtk.HBox()
+        self.entry = Gtk.Entry()
         self.entry.set_width_chars(entry_width)
         self.entry.set_activates_default(True)
         self.entry.set_text(init_text)
-        hb2.pack_start(self.entry, False)
-        vb.pack_start(hb2)
+        hb2.pack_start(self.entry, False, True, 0)
+        vb.pack_start(hb2, True, True, 0)
 
-        self.vbox.pack_start(vb)
+        self.vbox.pack_start(vb, True, True, 0)
         self.vbox.show_all()
 
     def get_text(self):
         return self.entry.get_text()
 
 
-class ProgressBarDialog(gtk.Window):
-    """ Defines a simple, non-blocking dialog window for displaying a progress bar
-    and a cancel button. """
-
+class ProgressBarDialog(Gtk.Window):
+    """
+    Defines a simple, non-blocking dialog window for displaying a progress bar
+    and a cancel button.
+    """
     def __init__(self, parent=None, msg='Progress...', title=''):
-        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+        Gtk.Window.__init__(self, Gtk.WindowType.TOPLEVEL)
         self.connect('delete-event', self.deleteWindow)
 
         self.is_canceled = False
@@ -116,37 +131,38 @@ class ProgressBarDialog(gtk.Window):
         self.set_destroy_with_parent(True)
         self.set_modal(True)
 
-        self.vbox = gtk.VBox(False, 14)
+        self.vbox = Gtk.VBox(False, 14)
         self.vbox.set_border_width(14)
         self.add(self.vbox)
 
         # create the message label
-        label = gtk.Label(msg)
-        self.vbox.pack_start(label)
+        label = Gtk.Label(label=msg)
+        self.vbox.pack_start(label, True, True, 0)
 
         # create the progress bar
-        self.pb = gtk.ProgressBar()
-        self.pb.set_orientation(gtk.PROGRESS_LEFT_TO_RIGHT)
+        self.pb = Gtk.ProgressBar()
+        self.pb.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self.pb.set_inverted(False)
         self.pb.set_text('0%')
-        self.vbox.pack_start(self.pb)
+        self.vbox.pack_start(self.pb, True, True, 0)
 
         # create the 'Close' button
-        bbox = gtk.HButtonBox()
-        bbox.set_layout(gtk.BUTTONBOX_END)
-        self.button = gtk.Button('Cancel', gtk.STOCK_CANCEL)
+        bbox = Gtk.HButtonBox()
+        bbox.set_layout(Gtk.ButtonBoxStyle.END)
+        self.button = Gtk.Button('Cancel', Gtk.STOCK_CANCEL)
         self.button.connect('clicked', self.cancelPressed)
         bbox.add(self.button)
-        self.vbox.pack_start(bbox)
+        self.vbox.pack_start(bbox, True, True, 0)
 
         self.vbox.show_all()
-        self.set_position(gtk.WIN_POS_CENTER_ON_PARENT)
+        self.set_position(Gtk.WindowPosition.CENTER_ON_PARENT)
 
     def show(self):
-        gtk.Window.show(self)
+        Gtk.Window.show(self)
 
-        # give the dialog window a chance to display
-        while gtk.events_pending():
-           gtk.main_iteration(False)
+        # Give the dialog window a chance to display.
+        while Gtk.events_pending():
+           Gtk.main_iteration()
 
     def updateProgress(self, fraction):
         self.pb.set_fraction(fraction)
@@ -156,9 +172,9 @@ class ProgressBarDialog(gtk.Window):
         if fraction >= 1.0:
             self.button.set_sensitive(False)
 
-        # give the progress bar a chance to actually update its display
-        while gtk.events_pending():
-           gtk.main_iteration(False)
+        # Give the progress bar a chance to actually update its display.
+        while Gtk.events_pending():
+           Gtk.main_iteration()
 
     def getIsCanceled(self):
         return self.is_canceled
@@ -171,7 +187,6 @@ class ProgressBarDialog(gtk.Window):
         self.is_canceled = True
 
         return True
-
 
 
 if __name__ == '__main__':
